8f901eff15cab0998b18af48b3583606fa5f0f5d
aknj <aknjne@gmail.com>
2016-12-17 00:09:47 +0100

    refactor and add mode_2 functionality

--- js/index.js
+++ js/index.js
@@ -1,56 +1,55 @@
 window.onload = function() {
   var fileInput = document.getElementById('fileInput');
   var fileDisplayArea = document.getElementById('fileDisplayArea');
-
-  fileInput.addEventListener('change', e => {
-    var file = fileInput.files[0];
-
-    var reader = new FileReader();
-    reader.onload = (e => {
-      fileDisplayArea.value = reader.result;
-
-      var data = JSON.parse(reader.result);
-      data.forEach(item => {
-        console.log(item.age);
-      } )
-		});
-    reader.readAsText(file);
-
-	});
-
-  // after clicking submit
   var submitButton = document.getElementById('submitButton');
+  var resultsSection = document.getElementById('results');
 
-  submitButton.addEventListener('click', e => {
-    var resultsSection = document.getElementById('results');
+  function readFile(file, fn) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      fn(reader.result);
+    };
+    reader.readAsText(file);
+  }
 
-    var mode = document.querySelector('input[name=mode]:checked').value;
+  /**
+   * change event on the fileInput element
+   */
+  fileInput.addEventListener('change', e => {
+    var uploadedFile = fileInput.files[0];
+    readFile(uploadedFile, function(callback) {
+      fileDisplayArea.value = callback;
+      var dataAsText = callback;
+    });
 
-    // if (mode === "2") {
-    //   zadanie2();
-    // } else if (mode === "3") {
-    //   zadanie3();
-    // } else if (mode === "4") {
-    //   var minId = document.getElementById('minId').value;
-    //   // TO DO: checking if value is numerical? is this needed?
-    //   zadanie4(minId);
-    // }
+    /**
+     * click event on the submitButton element
+     * TO DO
+     */
+    submitButton.addEventListener('click', e => {
+      var mode = document.querySelector('input[name=mode]:checked').value;
+      console.log('click');
+      readFile(uploadedFile, function(callback) {
+        var dataAsText = callback;
 
-    function makeFunc() {
-      var minId = document.getElementById('minId').value;
-      function executeJob() {
-        console.log('tajne tajne', mode, minId);
-      }
-      return executeJob;
-    }
+        var data = JSON.parse(dataAsText);
+        var results = [];
 
-    var myFunc = makeFunc();
-    myFunc();
+        if (mode === "2") {
+          for (var i = 0; i < 10; i++) {
+            let liElem = document.createElement('li');
+            for (var key in data[i]) {
+              liElem.textContent += (key + ": " + data[i][key] + " | ");
+            }
+            resultsSection.firstElementChild.append(liElem);
+          }
+        } else if (mode === "3") {
+
+        } else if (mode === "4") {
+          var minId = document.getElementById('minId').value;
+        }
+
+      });
+    });
   });
-
-
 }
-
-// function zadanie2() {
-//
-// }
