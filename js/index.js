46ff2fae63ac90dd6e6aaf8a06fb2c868e1f4637
aknj <aknjne@gmail.com>
2016-12-16 02:42:40 +0100

    style and modify user interface

--- js/index.js
+++ js/index.js
@@ -1,23 +1,27 @@
 window.onload = function() {
-		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
+  var fileInput = document.getElementById('fileInput');
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var jsonType = /[a-z]+\/json/;
+  fileInput.addEventListener('change', e => {
+    var file = fileInput.files[0];
 
-      //console.log(file);
+    var reader = new FileReader();
+    reader.onload = (e => {
+      fileDisplayArea.value = reader.result;
 
-			var reader = new FileReader();
-      reader.onload = function(e) {
-				fileDisplayArea.innerText = reader.result;
-
-        var data = JSON.parse(reader.result);
-        data.forEach( item => {
-          console.log(item.age);
-        } )
-			}
-      reader.readAsText(file);
-
+      var data = JSON.parse(reader.result);
+      data.forEach(item => {
+        console.log(item.age);
+      } )
 		});
+    reader.readAsText(file);
+
+	});
+
+  // after clicking submit
+  var submitButton = document.getElementById('submitButton');
+
+  submitButton.addEventListener('click', e => {
+    console.log('button clicked!');
+  })
 }
